a45490b02fa9586b7d21e311cd41e19aef6efa83
minhhieu45 <vmhsky@gmail.com>
2022-12-28 11:14:08 +0700

    oke oke oke

--- src/config/config.js
+++ src/config/config.js
@@ -2,5 +2,5 @@
 module.exports={
     "facebook_key"      :     "440008220955669", //Điền App ID của bạn vào đây
     "facebook_secret"   :     "e8fb21cbb038d034d1d8d83e1735fd13", //Điền App Secret ở đây
-    "callback_url"      :     "http://minhhieu45.site/auth/facebook/callback"
+    "callback_url"      :     "https://minhhieu45.site/auth/facebook/callback"
   }
